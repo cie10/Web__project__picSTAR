18f56a2e91edb70a02546261564842bff3980b51
cie10 <111051264+cie10@users.noreply.github.com>
2023-11-22 02:44:40 +0900

    modal

--- src/pictures.js
+++ src/pictures.js
@@ -32,21 +32,21 @@ categories.addEventListener('click',(event) => {
     }, 250);
 });
 
-// const modal = document.querySelector(".modal");
-// const img = document.querySelector(".picture__img");
-// const modal_img = document.querySelector(".modal_content");
-// const span = document.querySelector(".close");
+const modal = document.querySelector(".modal");
+const img = document.querySelector(".picture__img");
+const modal_img = document.querySelector(".modal__content");
+const span = document.querySelector(".close");
 
-// img.addEventListener('click', ()=>{
-//   modalDisplay("block");
-//   modal_img.src = img.src;
-// });
-// span.addEventListener('click', ()=>{
-//   modalDisplay("none");
-// });
-// modal.addEventListener('click', ()=>{
-//   modalDisplay("none");
-// });
-// function modalDisplay(text){
-//   modal.style.display = text;
-// }
+img.addEventListener('click', ()=>{
+    modalDisplay("block");
+    modal_img.src = img.src;
+  });
+  span.addEventListener('click', ()=>{
+    modalDisplay("none");
+  });
+  modal.addEventListener('click', ()=>{
+    modalDisplay("none");
+  });
+  function modalDisplay(text){
+    modal.style.display = text;
+  }
