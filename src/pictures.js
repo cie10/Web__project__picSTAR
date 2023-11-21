dae6f714dc2d1dc415cc36faf30c1f78dec4fe74
cie10 <111051264+cie10@users.noreply.github.com>
2023-11-22 02:33:34 +0900

    중간점검

--- src/pictures.js
+++ src/pictures.js
@@ -31,3 +31,22 @@ categories.addEventListener('click',(event) => {
         picturesContainer.classList.remove('anim-out');
     }, 250);
 });
+
+// const modal = document.querySelector(".modal");
+// const img = document.querySelector(".picture__img");
+// const modal_img = document.querySelector(".modal_content");
+// const span = document.querySelector(".close");
+
+// img.addEventListener('click', ()=>{
+//   modalDisplay("block");
+//   modal_img.src = img.src;
+// });
+// span.addEventListener('click', ()=>{
+//   modalDisplay("none");
+// });
+// modal.addEventListener('click', ()=>{
+//   modalDisplay("none");
+// });
+// function modalDisplay(text){
+//   modal.style.display = text;
+// }
